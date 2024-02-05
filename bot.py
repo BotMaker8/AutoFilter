299e450f62dacc7441811eecb206677607da5ea6
BotMaker8 <154598184+BotMaker8@users.noreply.github.com>
2024-02-05 21:02:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,8 +25,8 @@ logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from database.ia_filterdb import Media
-from database.users_chats_db import db
+from db.ia_filterdb import Media
+from db.users_chats_db import db
 from info import *
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
